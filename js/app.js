e99c2e89aa530c0792ff68dbaf6926eb31332fc7
Nefelijm <sistemasnik20@gmail.com>
2017-11-12 16:04:03 -0500

    Subiendo primera version

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+window.onload = function() {
+ var one = document.getElementById('button'),
+ two = one.addEventListener('click',visuali);
+}
+
+ function  visuali(event) {
+  var contenedor = document.querySelector('.visualizando');
+  var texto = document.formulario.text.value;
+    contenedor.innerHTML = texto
+    if
+
+    texto = document.getElementsByName('text').value;
+if( texto == null ) {
+  return alert('ingrese texto porfavor');
+}
+  }
