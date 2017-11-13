610f175d028d2199814b3594d607fe56afb8423a
Nefelijm <sistemasnik20@gmail.com>
2017-11-13 11:21:53 -0500

    version tres

--- js/app.js
+++ js/app.js
@@ -1,19 +1,53 @@
-
+//El evento va cargar cuando el obejeto haya cargado
 window.onload = function() {
+  //obteniendo el input con el id
  var one = document.getElementById('button'),
- two = one.addEventListener('click',visuali);
+ //Agregamos el evento click al input y la funcion a realizarce
+ two = one.addEventListener('click',visualizing);
 }
-
- function  visuali(event) {
-  var contenedor = document.querySelector('.visualizando');
-  var texto = document.formulario.text.value;
-    contenedor.innerHTML = texto
-  if (texto == 0) {
-  alert("ingresa un texto ");
+//Creando la funcion que se dara al realizar el evento
+ function  visualizing(event) {
+//obteniendo el div donde se vizualizara el twitter con una clase
+  var container = document.querySelector('.visualizando');
+//obteniendo el valor de la etiqueta texarea con un id
+  var content = document.formulario.area.value;
+//al div donde se visualizara le agregaremos el contenido que obtutivimos del textarea
+    container.innerHTML = content
+//validando que no ingrese vacio, ni espacios en blanco
+  if (content == 0) {
+      document.getElementById('button').disabled = true;
+      alert('Ingrese un texto porfavor')
+    }
+    else {
+    document.getElementById('button').disable = true;
     }
   }
 
-var accountant = document.formulario.area
-    accountant.addEventListener("keydown", function(event) {
-      document.getElementById("contadorNumeros").innerHTML = accountant.value.length;
-    });
+// object.addEventListener("load", myScript);
+  var tweet = document.formulario.area;
+  tweet.addEventListener("keydown",function(){
+  document.getElementById("contador").innerHTML = tweet.value.length;
+     var limite = 140;
+      var colorazul = 120;
+      var coloramarillo = 130;
+      var newtweet = tweet.value.length
+      var azulNegro = document.getElementById('contador')
+      var boton = document.getElementById('button')
+      if (newtweet > limite) {
+        azulNegro.style.background = 'green';
+        boton.disabled = true;
+      }
+      else if (newtweet > coloramarillo) {
+        azulNegro.style.background = 'yellow';
+        boton.disabled = true;
+
+      }
+      else if (newtweet > colorazul) {
+        azulNegro.style.background = 'blue';
+        boton.disabled = false;
+      }
+     else {
+       azulNegro.style.background = 'orange';
+   }
+
+ });
